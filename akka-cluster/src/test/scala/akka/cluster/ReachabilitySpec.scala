ddtxra/akka

commit dc9fe4f19c4d4e23b9b8a7b9142e212d37e4f176

!clu #2307 Allow transition from unreachable to reachable
* Replace unreachable Set with Reachability table
* Unreachable members stay in member Set
* Downing a live member was moved it to the unreachable Set,
  and then removed from there by the leader. That will not
  work when flipping back to reachable, so a Down member must
  be detected as unreachable before beeing removed. Similar
  to Exiting. Member shuts down itself if it sees itself as
  Down.
* Flip back to reachable when failure detector monitors it as
  available again
* ReachableMember event
* Can't ignore gossip from aggregated unreachable (see SurviveNetworkInstabilitySpec)
* Make use of ReachableMember event in cluster router
* End heartbeat when acknowledged, EndHeartbeatAck
* Remove nr-of-end-heartbeats from conf
* Full reachability info in JMX cluster status
* Don't use interval after unreachable for AccrualFailureDetector history
* Add QuarantinedEvent to remoting, used for Reachability.Terminated
* Prune reachability table when all reachable
* Update documentation
* Performance testing and optimizations

akka-cluster/src/main/scala/akka/cluster/Member.scala
@@ -213,10 +213,9 @@ object MemberStatus {
  */
 @SerialVersionUID(1L)
 private[cluster] case class UniqueAddress(address: Address, uid: Int) extends Ordered[UniqueAddress] {
-  @transient
-  override lazy val hashCode = scala.util.hashing.MurmurHash3.productHash(this)
+  override def hashCode = uid
 
-  override def compare(that: UniqueAddress): Int = {
+  def compare(that: UniqueAddress): Int = {
     val result = Member.addressOrdering.compare(this.address, that.address)
     if (result == 0) if (this.uid < that.uid) -1 else if (this.uid == that.uid) 0 else 1
     else result

akka-cluster/src/main/scala/akka/cluster/Reachability.scala
@@ -1,0 +1,294 @@
+/**
+ * Copyright (C) 2009-2013 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.cluster
+
+import scala.annotation.tailrec
+import scala.collection.immutable
+import scala.collection.breakOut
+import akka.actor.Address
+
+/**
+ * INTERNAL API
+ */
+private[cluster] object Reachability {
+  val empty = new Reachability(Vector.empty, Map.empty)
+
+  def apply(records: immutable.IndexedSeq[Record], versions: Map[UniqueAddress, Long]): Reachability =
+    new Reachability(records, versions)
+
+  def create(records: immutable.Seq[Record], versions: Map[UniqueAddress, Long]): Reachability = records match {
+    case r: immutable.IndexedSeq[Record] ⇒ apply(r, versions)
+    case _                               ⇒ apply(records.toVector, versions)
+  }
+
+  @SerialVersionUID(1L)
+  case class Record(observer: UniqueAddress, subject: UniqueAddress, status: ReachabilityStatus, version: Long)
+
+  sealed trait ReachabilityStatus
+  @SerialVersionUID(1L) case object Reachable extends ReachabilityStatus
+  @SerialVersionUID(1L) case object Unreachable extends ReachabilityStatus
+  @SerialVersionUID(1L) case object Terminated extends ReachabilityStatus
+
+}
+
+/**
+ * INTERNAL API
+ *
+ * Immutable data structure that holds the reachability status of subject nodes as seen
+ * from observer nodes. Failure detector for the subject nodes exist on the
+ * observer nodes. Changes (reachable, unreachable, terminated) are only performed
+ * by observer nodes to its own records. Each change bumps the version number of the
+ * record, and thereby it is always possible to determine which record is newest when
+ * merging two instances.
+ *
+ * Aggregated status of a subject node is defined as (in this order):
+ * - Terminated if any observer node considers it as Terminated
+ * - Unreachable if any observer node considers it as Unreachable
+ * - Reachable otherwise, i.e. no observer node considers it as Unreachable
+ */
+@SerialVersionUID(1L)
+private[cluster] class Reachability private (
+  val records: immutable.IndexedSeq[Reachability.Record],
+  val versions: Map[UniqueAddress, Long]) extends Serializable {
+
+  import Reachability._
+
+  private class Cache {
+    val (observerRowsMap, allUnreachable, allTerminated) = {
+      if (records.isEmpty) {
+        val observerRowsMap = Map.empty[UniqueAddress, Map[UniqueAddress, Reachability.Record]]
+        val allTerminated = Set.empty[UniqueAddress]
+        val allUnreachable = Set.empty[UniqueAddress]
+        (observerRowsMap, allUnreachable, allTerminated)
+      } else {
+        val mapBuilder = scala.collection.mutable.Map.empty[UniqueAddress, Map[UniqueAddress, Reachability.Record]]
+        import scala.collection.mutable.SetBuilder
+        val terminatedBuilder = new SetBuilder[UniqueAddress, Set[UniqueAddress]](Set.empty)
+        val unreachableBuilder = new SetBuilder[UniqueAddress, Set[UniqueAddress]](Set.empty)
+
+        records foreach { r ⇒
+          val m = mapBuilder.get(r.observer) match {
+            case None    ⇒ Map(r.subject -> r)
+            case Some(m) ⇒ m.updated(r.subject, r)
+          }
+          mapBuilder += (r.observer -> m)
+
+          if (r.status == Unreachable) unreachableBuilder += r.subject
+          else if (r.status == Terminated) terminatedBuilder += r.subject
+        }
+
+        val observerRowsMap: Map[UniqueAddress, Map[UniqueAddress, Reachability.Record]] = mapBuilder.toMap
+        val allTerminated: Set[UniqueAddress] = terminatedBuilder.result()
+        val allUnreachable: Set[UniqueAddress] = unreachableBuilder.result() -- allTerminated
+
+        (observerRowsMap, allUnreachable, allTerminated)
+      }
+    }
+
+    val allUnreachableOrTerminated: Set[UniqueAddress] =
+      if (allTerminated.isEmpty) allUnreachable
+      else allUnreachable ++ allTerminated
+
+  }
+
+  @transient private lazy val cache = new Cache
+
+  private def observerRows(observer: UniqueAddress): Option[Map[UniqueAddress, Reachability.Record]] =
+    cache.observerRowsMap.get(observer)
+
+  def unreachable(observer: UniqueAddress, subject: UniqueAddress): Reachability =
+    change(observer, subject, Unreachable)
+
+  def reachable(observer: UniqueAddress, subject: UniqueAddress): Reachability =
+    change(observer, subject, Reachable)
+
+  def terminated(observer: UniqueAddress, subject: UniqueAddress): Reachability =
+    change(observer, subject, Terminated)
+
+  private def currentVersion(observer: UniqueAddress): Long = versions.get(observer) match {
+    case None    ⇒ 0
+    case Some(v) ⇒ v
+  }
+
+  private def nextVersion(observer: UniqueAddress): Long = currentVersion(observer) + 1
+
+  private def change(observer: UniqueAddress, subject: UniqueAddress, status: ReachabilityStatus): Reachability = {
+    val v = nextVersion(observer)
+    val newVersions = versions.updated(observer, v)
+    val newRecord = Record(observer, subject, status, v)
+    observerRows(observer) match {
+      case None if status == Reachable ⇒ this
+      case None ⇒
+        new Reachability(records :+ newRecord, newVersions)
+
+      case Some(oldObserverRows) ⇒
+
+        oldObserverRows.get(subject) match {
+          case None ⇒
+            if (status == Reachable && oldObserverRows.forall { case (_, r) ⇒ r.status == Reachable }) {
+              // all Reachable, prune by removing the records of the observer, and bump the version
+              new Reachability(records.filterNot(_.observer == observer), newVersions)
+            } else
+              new Reachability(records :+ newRecord, newVersions)
+          case Some(oldRecord) ⇒
+            if (oldRecord.status == Terminated || oldRecord.status == status)
+              this
+            else {
+              if (status == Reachable && oldObserverRows.forall { case (_, r) ⇒ r.status == Reachable || r.subject == subject }) {
+                // all Reachable, prune by removing the records of the observer, and bump the version
+                new Reachability(records.filterNot(_.observer == observer), newVersions)
+              } else {
+                val newRecords = records.updated(records.indexOf(oldRecord), newRecord)
+                new Reachability(newRecords, newVersions)
+              }
+            }
+        }
+    }
+  }
+
+  def merge(allowed: immutable.Set[UniqueAddress], other: Reachability): Reachability = {
+    val recordBuilder = new immutable.VectorBuilder[Record]
+    recordBuilder.sizeHint(math.max(this.records.size, other.records.size))
+    var newVersions = versions
+    allowed foreach { observer ⇒
+      val observerVersion1 = this.currentVersion(observer)
+      val observerVersion2 = other.currentVersion(observer)
+
+      (this.observerRows(observer), other.observerRows(observer)) match {
+        case (None, None) ⇒
+        case (Some(rows1), Some(rows2)) ⇒
+          mergeObserverRows(rows1, rows2, observerVersion1, observerVersion2, recordBuilder)
+        case (Some(rows1), None) ⇒
+          recordBuilder ++= rows1.collect { case (_, r) if r.version > observerVersion2 ⇒ r }
+        case (None, Some(rows2)) ⇒
+          recordBuilder ++= rows2.collect { case (_, r) if r.version > observerVersion1 ⇒ r }
+      }
+
+      if (observerVersion2 > observerVersion1)
+        newVersions += (observer -> observerVersion2)
+    }
+
+    new Reachability(recordBuilder.result(), newVersions)
+  }
+
+  private def mergeObserverRows(
+    rows1: Map[UniqueAddress, Reachability.Record], rows2: Map[UniqueAddress, Reachability.Record],
+    observerVersion1: Long, observerVersion2: Long,
+    recordBuilder: immutable.VectorBuilder[Record]): Unit = {
+
+    val allSubjects = rows1.keySet ++ rows2.keySet
+    allSubjects foreach { subject ⇒
+      (rows1.get(subject), rows2.get(subject)) match {
+        case (Some(r1), Some(r2)) ⇒
+          recordBuilder += (if (r1.version > r2.version) r1 else r2)
+        case (Some(r1), None) ⇒
+          if (r1.version > observerVersion2)
+            recordBuilder += r1
+        case (None, Some(r2)) ⇒
+          if (r2.version > observerVersion1)
+            recordBuilder += r2
+        case (None, None) ⇒
+          throw new IllegalStateException(s"Unexpected [$subject]")
+      }
+    }
+  }
+
+  def remove(nodes: Iterable[UniqueAddress]): Reachability = {
+    val nodesSet = nodes.to[immutable.HashSet]
+    val newRecords = records.filterNot(r ⇒ nodesSet(r.observer) || nodesSet(r.subject))
+    if (newRecords.size == records.size) this
+    else {
+      val newVersions = versions -- nodes
+      Reachability(newRecords, newVersions)
+    }
+  }
+
+  def status(observer: UniqueAddress, subject: UniqueAddress): ReachabilityStatus =
+    observerRows(observer) match {
+      case None ⇒ Reachable
+      case Some(observerRows) ⇒ observerRows.get(subject) match {
+        case None         ⇒ Reachable
+        case Some(record) ⇒ record.status
+      }
+    }
+
+  def status(node: UniqueAddress): ReachabilityStatus =
+    if (cache.allTerminated(node)) Terminated
+    else if (cache.allUnreachable(node)) Unreachable
+    else Reachable
+
+  def isReachable(node: UniqueAddress): Boolean = isAllReachable || !allUnreachableOrTerminated.contains(node)
+
+  def isReachable(observer: UniqueAddress, subject: UniqueAddress): Boolean =
+    status(observer, subject) == Reachable
+
+  def isAllReachable: Boolean = records.isEmpty
+
+  /**
+   * Doesn't include terminated
+   */
+  def allUnreachable: Set[UniqueAddress] = cache.allUnreachable
+
+  def allUnreachableOrTerminated: Set[UniqueAddress] = cache.allUnreachableOrTerminated
+
+  /**
+   * Doesn't include terminated
+   */
+  def allUnreachableFrom(observer: UniqueAddress): Set[UniqueAddress] =
+    observerRows(observer) match {
+      case None ⇒ Set.empty
+      case Some(observerRows) ⇒
+        observerRows.collect {
+          case (subject, record) if record.status == Unreachable ⇒ subject
+        }(breakOut)
+    }
+
+  def observersGroupedByUnreachable: Map[UniqueAddress, Set[UniqueAddress]] = {
+    records.groupBy(_.subject).collect {
+      case (subject, records) if records.exists(_.status == Unreachable) ⇒
+        val observers: Set[UniqueAddress] =
+          records.collect { case r if r.status == Unreachable ⇒ r.observer }(breakOut)
+        (subject -> observers)
+    }
+  }
+
+  def allObservers: Set[UniqueAddress] = versions.keySet
+
+  def recordsFrom(observer: UniqueAddress): immutable.IndexedSeq[Record] = {
+    observerRows(observer) match {
+      case None       ⇒ Vector.empty
+      case Some(rows) ⇒ rows.valuesIterator.toVector
+    }
+  }
+
+  // only used for testing
+  override def hashCode: Int = versions.hashCode
+
+  // only used for testing
+  override def equals(obj: Any): Boolean = obj match {
+    case other: Reachability ⇒
+      records.size == other.records.size && versions == versions &&
+        cache.observerRowsMap == other.cache.observerRowsMap
+    case _ ⇒ false
+  }
+
+  override def toString: String = {
+    val rows = for {
+      observer ← versions.keys.toSeq.sorted
+      rowsOption = observerRows(observer)
+      if rowsOption.isDefined // compilation err for subject <- rowsOption
+      rows = rowsOption.get
+      subject ← rows.keys.toSeq.sorted
+    } yield {
+      val record = rows(subject)
+      val aggregated = status(subject)
+      s"${observer.address} -> ${subject.address}: ${record.status} [$aggregated] (${record.version})"
+      ""
+    }
+
+    rows.mkString(", ")
+  }
+
+}
+

akka-cluster/src/multi-jvm/scala/akka/cluster/MultiNodeClusterSpec.scala
@@ -38,12 +38,13 @@ object MultiNodeClusterSpec {
       jmx.enabled                         = off
       gossip-interval                     = 200 ms
       leader-actions-interval             = 200 ms
-      unreachable-nodes-reaper-interval   = 200 ms
+      unreachable-nodes-reaper-interval   = 500 ms
       periodic-tasks-initial-delay        = 300 ms
       publish-stats-interval              = 0 s # always, when it happens
-      failure-detector.heartbeat-interval = 400 ms
+      failure-detector.heartbeat-interval = 500 ms
     }
     akka.loglevel = INFO
+    akka.log-dead-letters = off
     akka.log-dead-letters-during-shutdown = off
     akka.remote.log-remote-lifecycle-events = off
     akka.loggers = ["akka.testkit.TestEventListener"]
@@ -114,8 +115,10 @@ trait MultiNodeClusterSpec extends Suite with STMultiNodeSpec with WatchedByCoro
         classOf[InternalClusterAction.Tick],
         classOf[akka.actor.PoisonPill],
         classOf[akka.dispatch.sysmsg.DeathWatchNotification],
-        akka.remote.transport.AssociationHandle.Disassociated.getClass,
-        akka.remote.transport.ActorTransportAdapter.DisassociateUnderlying.getClass,
+        classOf[akka.remote.transport.AssociationHandle.Disassociated],
+        //        akka.remote.transport.AssociationHandle.Disassociated.getClass,
+        classOf[akka.remote.transport.ActorTransportAdapter.DisassociateUnderlying],
+        //        akka.remote.transport.ActorTransportAdapter.DisassociateUnderlying.getClass,
         classOf[akka.remote.transport.AssociationHandle.InboundPayload])(sys)
 
     }
@@ -124,6 +127,10 @@ trait MultiNodeClusterSpec extends Suite with STMultiNodeSpec with WatchedByCoro
   def muteMarkingAsUnreachable(sys: ActorSystem = system): Unit =
     if (!sys.log.isDebugEnabled)
       sys.eventStream.publish(Mute(EventFilter.error(pattern = ".*Marking.* as UNREACHABLE.*")))
+
+  def muteMarkingAsReachable(sys: ActorSystem = system): Unit =
+    if (!sys.log.isDebugEnabled)
+      sys.eventStream.publish(Mute(EventFilter.info(pattern = ".*Marking.* as REACHABLE.*")))
 
   override def afterAll(): Unit = {
     if (!log.isDebugEnabled) {
@@ -292,7 +299,8 @@ trait MultiNodeClusterSpec extends Suite with STMultiNodeSpec with WatchedByCoro
     }
   }
 
-  def awaitAllReachable(): Unit = awaitAssert(clusterView.unreachableMembers.isEmpty)
+  def awaitAllReachable(): Unit =
+    awaitAssert(clusterView.unreachableMembers must be(Set.empty))
 
   /**
    * Wait until the specified nodes have seen the same gossip overview.

akka-cluster/src/test/scala/akka/cluster/GossipSpec.scala
@@ -47,40 +47,17 @@ class GossipSpec extends WordSpec with MustMatchers {
 
     }
 
-    "merge unreachable by status priority" in {
-      val g1 = Gossip(members = Gossip.emptyMembers, overview = GossipOverview(unreachable = Set(a1, b1, c1, d1)))
-      val g2 = Gossip(members = Gossip.emptyMembers, overview = GossipOverview(unreachable = Set(a2, b2, c2, d2)))
+    "merge unreachable" in {
+      val r1 = Reachability.empty.unreachable(b1.uniqueAddress, a1.uniqueAddress).unreachable(b1.uniqueAddress, c1.uniqueAddress)
+      val g1 = Gossip(members = SortedSet(a1, b1, c1), overview = GossipOverview(reachability = r1))
+      val r2 = Reachability.empty.unreachable(a1.uniqueAddress, d1.uniqueAddress)
+      val g2 = Gossip(members = SortedSet(a1, b1, c1, d1), overview = GossipOverview(reachability = r2))
 
       val merged1 = g1 merge g2
-      merged1.overview.unreachable must be(Set(a2, b2, c1, d2))
-      merged1.overview.unreachable.toSeq.sorted.map(_.status) must be(Seq(Up, Removed, Leaving, Removed))
+      merged1.overview.reachability.allUnreachable must be(Set(a1.uniqueAddress, c1.uniqueAddress, d1.uniqueAddress))
 
       val merged2 = g2 merge g1
-      merged2.overview.unreachable must be(Set(a2, b2, c1, d2))
-      merged2.overview.unreachable.toSeq.sorted.map(_.status) must be(Seq(Up, Removed, Leaving, Removed))
-
-    }
-
-    "merge by excluding unreachable from members" in {
-      val g1 = Gossip(members = SortedSet(a1, b1), overview = GossipOverview(unreachable = Set(c1, d1)))
-      val g2 = Gossip(members = SortedSet(a2, c2), overview = GossipOverview(unreachable = Set(b2, d2)))
-
-      val merged1 = g1 merge g2
-      merged1.members must be(SortedSet(a2))
-      merged1.members.toSeq.map(_.status) must be(Seq(Up))
-      merged1.overview.unreachable must be(Set(b2, c1, d2))
-      merged1.overview.unreachable.toSeq.sorted.map(_.status) must be(Seq(Removed, Leaving, Removed))
-
-      val merged2 = g2 merge g1
-      merged2.members must be(SortedSet(a2))
-      merged2.members.toSeq.map(_.status) must be(Seq(Up))
-      merged2.overview.unreachable must be(Set(b2, c1, d2))
-      merged2.overview.unreachable.toSeq.sorted.map(_.status) must be(Seq(Removed, Leaving, Removed))
-
-    }
-
-    "not have node in both members and unreachable" in intercept[IllegalArgumentException] {
-      Gossip(members = SortedSet(a1, b1), overview = GossipOverview(unreachable = Set(b2)))
+      merged2.overview.reachability.allUnreachable must be(merged1.overview.reachability.allUnreachable)
     }
 
     "not have live members with wrong status" in intercept[IllegalArgumentException] {
@@ -121,9 +98,11 @@ class GossipSpec extends WordSpec with MustMatchers {
 
     "know who is youngest" in {
       // a2 and e1 is Joining
-      val g1 = Gossip(members = SortedSet(a2, b1.copyUp(3)), overview = GossipOverview(unreachable = Set(e1)))
+      val g1 = Gossip(members = SortedSet(a2, b1.copyUp(3), e1), overview = GossipOverview(reachability =
+        Reachability.empty.unreachable(a2.uniqueAddress, e1.uniqueAddress)))
       g1.youngestMember must be(b1)
-      val g2 = Gossip(members = SortedSet(a2), overview = GossipOverview(unreachable = Set(b1.copyUp(3), e1)))
+      val g2 = Gossip(members = SortedSet(a2, b1.copyUp(3), e1), overview = GossipOverview(reachability =
+        Reachability.empty.unreachable(a2.uniqueAddress, b1.uniqueAddress).unreachable(a2.uniqueAddress, e1.uniqueAddress)))
       g2.youngestMember must be(b1)
       val g3 = Gossip(members = SortedSet(a2, b1.copyUp(3), e2.copyUp(4)))
       g3.youngestMember must be(e2)

commit ad81d12a50c82fe9af582af2b9c56c9da6ca7b04

=clu #3603 Handle removed member in Gossip and Reachability merge
* It was a regression introduced in dc9fe4f
* Two problems:
  1) Gossip merge could pop back removed member (was previously
     covered by the filter of unreachable)
  2) Reachability merge didn't handle all cases for removed member,
     i.e. when node not in allowed set

akka-actor/src/main/scala/akka/util/BoundedBlockingQueue.scala
@@ -104,8 +104,8 @@ class BoundedBlockingQueue[E <: AnyRef](
       @tailrec def pollElement(remainingNanos: Long): E = {
         backing.poll() match {
           case null if remainingNanos <= 0 ⇒ null.asInstanceOf[E]
-          case null ⇒ pollElement(notEmpty.awaitNanos(remainingNanos))
-          case e 	⇒ {
+          case null                        ⇒ pollElement(notEmpty.awaitNanos(remainingNanos))
+          case e ⇒ {
             notFull.signal()
             e
           }
@@ -120,7 +120,7 @@ class BoundedBlockingQueue[E <: AnyRef](
     try {
       backing.poll() match {
         case null ⇒ null.asInstanceOf[E]
-        case e    ⇒
+        case e ⇒
           notFull.signal()
           e
       }

akka-cluster/src/main/scala/akka/cluster/Member.scala
@@ -127,7 +127,13 @@ object Member {
     val groupedByAddress = (a.toSeq ++ b.toSeq).groupBy(_.uniqueAddress)
     // pick highest MemberStatus
     (Member.none /: groupedByAddress) {
-      case (acc, (_, members)) ⇒ acc + members.reduceLeft(highestPriorityOf)
+      case (acc, (_, members)) ⇒
+        if (members.size == 2) acc + members.reduceLeft(highestPriorityOf)
+        else {
+          val m = members.head
+          if (Gossip.removeUnreachableWithMemberStatus(m.status)) acc // removed
+          else acc + m
+        }
     }
   }
 

akka-cluster/src/main/scala/akka/cluster/Reachability.scala
@@ -158,38 +158,43 @@ private[cluster] class Reachability private (
       (this.observerRows(observer), other.observerRows(observer)) match {
         case (None, None) ⇒
         case (Some(rows1), Some(rows2)) ⇒
-          mergeObserverRows(rows1, rows2, observerVersion1, observerVersion2, recordBuilder)
+          mergeObserverRows(allowed, rows1, rows2, observerVersion1, observerVersion2, recordBuilder)
         case (Some(rows1), None) ⇒
-          recordBuilder ++= rows1.collect { case (_, r) if r.version > observerVersion2 ⇒ r }
+          recordBuilder ++= rows1.collect { case (_, r) if r.version > observerVersion2 && allowed(r.subject) ⇒ r }
         case (None, Some(rows2)) ⇒
-          recordBuilder ++= rows2.collect { case (_, r) if r.version > observerVersion1 ⇒ r }
+          recordBuilder ++= rows2.collect { case (_, r) if r.version > observerVersion1 && allowed(r.subject) ⇒ r }
       }
 
       if (observerVersion2 > observerVersion1)
         newVersions += (observer -> observerVersion2)
     }
 
+    newVersions = newVersions.filterNot { case (k, _) ⇒ !allowed(k) }
+
     new Reachability(recordBuilder.result(), newVersions)
   }
 
   private def mergeObserverRows(
+    allowed: immutable.Set[UniqueAddress],
     rows1: Map[UniqueAddress, Reachability.Record], rows2: Map[UniqueAddress, Reachability.Record],
     observerVersion1: Long, observerVersion2: Long,
     recordBuilder: immutable.VectorBuilder[Record]): Unit = {
 
     val allSubjects = rows1.keySet ++ rows2.keySet
     allSubjects foreach { subject ⇒
-      (rows1.get(subject), rows2.get(subject)) match {
-        case (Some(r1), Some(r2)) ⇒
-          recordBuilder += (if (r1.version > r2.version) r1 else r2)
-        case (Some(r1), None) ⇒
-          if (r1.version > observerVersion2)
-            recordBuilder += r1
-        case (None, Some(r2)) ⇒
-          if (r2.version > observerVersion1)
-            recordBuilder += r2
-        case (None, None) ⇒
-          throw new IllegalStateException(s"Unexpected [$subject]")
+      if (allowed(subject)) {
+        (rows1.get(subject), rows2.get(subject)) match {
+          case (Some(r1), Some(r2)) ⇒
+            recordBuilder += (if (r1.version > r2.version) r1 else r2)
+          case (Some(r1), None) ⇒
+            if (r1.version > observerVersion2)
+              recordBuilder += r1
+          case (None, Some(r2)) ⇒
+            if (r2.version > observerVersion1)
+              recordBuilder += r2
+          case (None, None) ⇒
+            throw new IllegalStateException(s"Unexpected [$subject]")
+        }
       }
     }
   }
@@ -284,7 +289,6 @@ private[cluster] class Reachability private (
       val record = rows(subject)
       val aggregated = status(subject)
       s"${observer.address} -> ${subject.address}: ${record.status} [$aggregated] (${record.version})"
-      ""
     }
 
     rows.mkString(", ")

akka-cluster/src/multi-jvm/scala/akka/cluster/MultiNodeClusterSpec.scala
@@ -287,7 +287,7 @@ trait MultiNodeClusterSpec extends Suite with STMultiNodeSpec with WatchedByCoro
   def awaitMembersUp(
     numberOfMembers: Int,
     canNotBePartOfMemberRing: Set[Address] = Set.empty,
-    timeout: FiniteDuration = 20.seconds): Unit = {
+    timeout: FiniteDuration = 25.seconds): Unit = {
     within(timeout) {
       if (!canNotBePartOfMemberRing.isEmpty) // don't run this on an empty set
         awaitAssert(canNotBePartOfMemberRing foreach (a ⇒ clusterView.members.map(_.address) must not contain (a)))

akka-cluster/src/test/scala/akka/cluster/GossipSpec.scala
@@ -22,7 +22,6 @@ class GossipSpec extends WordSpec with MustMatchers {
   val c2 = TestMember(c1.address, Up)
   val c3 = TestMember(c1.address, Exiting)
   val d1 = TestMember(Address("akka.tcp", "sys", "d", 2552), Leaving)
-  val d2 = TestMember(d1.address, Removed)
   val e1 = TestMember(Address("akka.tcp", "sys", "e", 2552), Joining)
   val e2 = TestMember(e1.address, Up)
   val e3 = TestMember(e1.address, Down)
@@ -58,6 +57,22 @@ class GossipSpec extends WordSpec with MustMatchers {
 
       val merged2 = g2 merge g1
       merged2.overview.reachability.allUnreachable must be(merged1.overview.reachability.allUnreachable)
+    }
+
+    "merge members by removing removed members" in {
+      // c3 removed
+      val r1 = Reachability.empty.unreachable(b1.uniqueAddress, a1.uniqueAddress)
+      val g1 = Gossip(members = SortedSet(a1, b1), overview = GossipOverview(reachability = r1))
+      val r2 = r1.unreachable(b1.uniqueAddress, c3.uniqueAddress)
+      val g2 = Gossip(members = SortedSet(a1, b1, c3), overview = GossipOverview(reachability = r2))
+
+      val merged1 = g1 merge g2
+      merged1.members must be(SortedSet(a1, b1))
+      merged1.overview.reachability.allUnreachable must be(Set(a1.uniqueAddress))
+
+      val merged2 = g2 merge g1
+      merged2.overview.reachability.allUnreachable must be(merged1.overview.reachability.allUnreachable)
+      merged2.members must be(merged1.members)
     }
 
     "not have live members with wrong status" in intercept[IllegalArgumentException] {

akka-cluster/src/test/scala/akka/cluster/ReachabilitySpec.scala
@@ -148,6 +148,30 @@ class ReachabilitySpec extends WordSpec with MustMatchers {
       merged2.records.toSet must be(merged.records.toSet)
     }
 
+    "merge by taking allowed set into account" in {
+      val r1 = Reachability.empty.unreachable(nodeB, nodeA).unreachable(nodeC, nodeD)
+      val r2 = r1.reachable(nodeB, nodeA).unreachable(nodeD, nodeE).unreachable(nodeC, nodeA)
+      // nodeD not in allowed set
+      val allowed = Set(nodeA, nodeB, nodeC, nodeE)
+      val merged = r1.merge(allowed, r2)
+
+      merged.status(nodeB, nodeA) must be(Reachable)
+      merged.status(nodeC, nodeA) must be(Unreachable)
+      merged.status(nodeC, nodeD) must be(Reachable)
+      merged.status(nodeD, nodeE) must be(Reachable)
+      merged.status(nodeE, nodeA) must be(Reachable)
+
+      merged.isReachable(nodeA) must be(false)
+      merged.isReachable(nodeD) must be(true)
+      merged.isReachable(nodeE) must be(true)
+
+      merged.versions.keySet must be(Set(nodeB, nodeC))
+
+      val merged2 = r2.merge(allowed, r1)
+      merged2.records.toSet must be(merged.records.toSet)
+      merged2.versions must be(merged.versions)
+    }
+
     "merge correctly after pruning" in {
       val r1 = Reachability.empty.unreachable(nodeB, nodeA).unreachable(nodeC, nodeD)
       val r2 = r1.unreachable(nodeA, nodeE)